14f125d2c97bf900b344c3a5066713b1589aaa8d
DESKTOP-M6HM8SK\user <fourbasic@gmail.com>
2019-03-10 21:58:16 +0900

    【制作】ExampleTest.phpに基本テストを実行させる。

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -16,6 +16,88 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        //200番台 正常処理
+        //$response->assertStatus(200);
+
+        //このアドレスは一時的に別のアドレスにおいています。
+        $response->assertStatus(302);
     }
+
+    public function testBasicTrue()
+    {
+        $tf = true;
+        $this->assertTrue($tf);
+    }
+
+    public function testBasicFalse()
+    {
+        $tf = false;
+        $this->assertFalse($tf);
+    }
+
+    public $bango = 55;
+    public $kara = "";
+    public $null = null;
+
+    public function testBasicEqual()
+    {
+        $this->assertEquals(55,$this->bango);
+    }
+
+    public function testBasicNotEqual()
+    {
+        $this->assertNotEquals(35,$this->bango);
+    }
+
+    public function testBasicLessThan()
+    {
+        $this->assertLessThan(56,$this->bango);
+    }
+
+    public function testBasicLessThanOrEqual()
+    {
+        $this->assertLessThanOrEqual(55,$this->bango);
+    }
+
+    public function testBasicGreaterThan()
+    {
+        $this->assertGreaterThan(54,$this->bango);
+    }
+
+    public function testBasicGreaterThanOrEqual()
+    {
+        $this->assertGreaterThanOrEqual(55,$this->bango);
+    }
+
+    public function testBasicEmpty()
+    {
+        $this->assertEmpty($this->kara);
+    }
+
+    public function testBasicNotEmpty()
+    {
+        $this->assertNotEmpty(55);
+    }
+
+    public function testBasicNull()
+    {
+        $this->assertNull($this->null);
+    }
+
+    public function testBasicNotNull()
+    {
+        $this->assertNotNull(55);
+    }
+
+    public function testBasicStringStartsWith()
+    {
+        $this->assertStringStartsWith(5,$this->bango);
+    }
+
+    public function testBasicStringEndsWith()
+    {
+        $this->assertStringEndsWith(5,$this->bango);
+    }
+
+
 }
